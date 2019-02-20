95422473c49349628ac5b49028b8fcc7f091857f
Khanh Vu <khanhvu@Khanhs-MacBook-Pro.local>
2019-02-19 22:10:36 -0500

    test

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -10,7 +10,17 @@ connection.connect()
 router.post('/addTask', (req, res)=>{
 	const taskName = req.body.taskName
 	const taskDate = req.body.taskDate
-	res.json({taskName, taskDate})
+	const insertQuery = `INSERT INTO tasks(taskName,taskDate)
+		VALUES (?,?)`;
+		connection.query(insertQuery,[taskName,taskDate],(error, results)=>{
+			if (error){throw error}
+			const getTasksQuery = `SELECT * FROM tasks`
+			connection.query(getTasksQuery,(error2, results2)=>{
+				if (error2){throw error2};
+				res.json(results2)
+			})
+		})
+	// res.json({taskName, taskDate})
 })
 
 module.exports = router;
